9dc2d6862ba072450c21eddf9c72371e6ddffdf9
Henning Jansen <henning.jansen@jansenh.no>
2022-11-23 13:15:00 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,61 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Mute splash-screen
+(setq inhibit-startup-message t)
+
+(setq user-full-name "Henning Jansen"
+      user-mail-address "henning.jansen@jansenh.no")
+
+;; Set path to dependencies -------------------------------------------
+(setq warning-suppress-log-types '((package reinitialization)))
+(package-initialize)
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path settings-dir)
+(add-to-list 'load-path site-lisp-dir)
+;; --------------------------------------------------------------------
+
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Write all autosave files in the tmp dir
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Don't write lock-files
+(setq create-lockfiles nil)
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+;; ---------------------------------------------------------------------
+
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
+
+;; Package Management --------------------------------------------------
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+(unless (assoc-default "nongnu" package-archives)
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
+
+
